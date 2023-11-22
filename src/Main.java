19ced71d58627b477189dffa2c5884ef9040ea7e
Gean Victor <geanvictorduque@gmail.com>
2023-11-22 16:47:09 -0300

    feat: Add seller class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "teste");
         System.out.println(department);
+
+        Seller seller = new Seller(2, "Gean", "gean@email.com", new Date(), 3000.0, department);
+        System.out.println(seller);
     }
 }
