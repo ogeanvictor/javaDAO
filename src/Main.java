ec0ccb27b6610b9bd45f5449571297e9ff9f0811
Gean Victor <geanvictorduque@gmail.com>
2023-11-23 12:45:43 -0300

    feat: Add seller findById method

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "teste");
-        System.out.println(department);
-
-        Seller seller = new Seller(2, "Gean", "gean@email.com", new Date(), 3000.0, department);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
